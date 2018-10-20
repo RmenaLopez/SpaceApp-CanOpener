e42442c70ac48fc9c17c45cd1f3331424cee3df5
Rafael Mena <rmena@NSL-RMENAs-MacBook-Pro.local>
2018-10-20 00:28:18 -0700

    first commit

---
+++ src/main/java/com/spaceapps/canopener/SpaceAppCanOpener/SpaceAppCanOpenerApplication.java
@@ -0,0 +1,12 @@
+package com.spaceapps.canopener.SpaceAppCanOpener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceAppCanOpenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceAppCanOpenerApplication.class, args);
+	}
+}
