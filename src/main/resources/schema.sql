75b78cb3fa916d431c45bd24778fcc49bed76f18
Rafael Mena <rmena@NSL-RMENAs-MacBook-Pro.local>
2018-10-21 05:22:18 -0700

    more sql changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 -- schema.sql
-CREATE TABLE natural_disasters_record(
-  id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  disaster_name varchar (255) NOT NULL
-);
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='natural_disasters_record' AND xtype='U')
+  CREATE TABLE natural_disasters_record(
+    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    disaster_name varchar (255) NOT NULL
+  );
