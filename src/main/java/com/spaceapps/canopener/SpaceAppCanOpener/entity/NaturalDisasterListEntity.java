eaf0ebd95d786d35581d7a898e8cfaef5fc7666b
Rafael Mena <rmena@NSL-RMENAs-MacBook-Pro.local>
2018-10-21 04:42:24 -0700

    create disaster table

---
+++ src/main/java/com/spaceapps/canopener/SpaceAppCanOpener/entity/NaturalDisasterListEntity.java
@@ -0,0 +1,32 @@
+package com.spaceapps.canopener.SpaceAppCanOpener.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "natural_disasters_record")
+public class NaturalDisasterListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "disaster_name", nullable = false, updatable = false, unique = true)
+    private String disasterName;
+
+    public NaturalDisasterListEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDisasterName() {
+        return disasterName;
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Flood');
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Comet');
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Earthquake');
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Drought');
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Wild fire');
+INSERT INTO natural_disasters_record (disaster_name) VALUES ('Avalanche');
