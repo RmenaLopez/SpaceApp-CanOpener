d849aab3927c697eaf1783ad5f57599c1c55930f
Rafael Mena <rmena@NSL-RMENAs-MacBook-Pro.local>
2018-10-21 05:26:10 -0700

    schema drop table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 -- schema.sql
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='natural_disasters_record' AND xtype='U')
+IF OBJECT_ID('natural_disasters_record', 'U') IS NOT NULL
+  DROP TABLE natural_disasters_record;
+
   CREATE TABLE natural_disasters_record(
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     disaster_name varchar (255) NOT NULL
